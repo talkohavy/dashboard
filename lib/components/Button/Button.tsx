08210f1283a6fdb79ecef0dff546693d3078ece0
Tal Kohavy <talkohavy@gmail.com>
2024-08-09 00:38:58 +0300

    gave the Button better styles

--- lib/components/Button/Button.tsx
+++ lib/components/Button/Button.tsx
@@ -1,10 +1,11 @@
+import clsx from 'clsx';
 import styles from './Button.module.scss';
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button(props: ButtonProps) {
   return (
-    <button {...props} className={`${props.className} ${styles.button}`}>
+    <button {...props} className={clsx(props.className, styles.button)}>
       Click Me
     </button>
   );
